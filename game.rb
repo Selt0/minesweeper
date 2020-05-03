b638edf348979e2ed8709ddfa238f5baa1d35ee4
Selt0 <chewedgum12@gmail.com>
2020-05-03 12:27:50 -0500

    Add get_move to replace get_pos and get_val

--- game.rb
+++ game.rb
@@ -3,14 +3,14 @@ require_relative "board"
 
 class Minesweeper
   LAYOUTS = {
-    small: {grid_size: 9, num_bombs: 10}
-    medium: {grid_size: 16, num_bombs: 40}
+    small: {grid_size: 9, num_bombs: 10},
+    medium: {grid_size: 16, num_bombs: 40},
     large: {grid_size: 32, num_bombs: 160}
 }.freeze
 
   attr_reader :board
 
-  def initialize(size)
+  def initialize(size = :small)
     layout = LAYOUTS[size]
     @board = Board.new(layout[:grid_size], layout[:num_bombs])
   end
@@ -30,51 +30,46 @@ class Minesweeper
     end
   end
 
-  def get_pos
+  def get_move
     pos = nil
-    until pos && valid_pos?(pos)
-      puts "Enter a position on the board (e.g., '3 4')"
-      print "> "
+    action_type = nil
 
-      begin
-        pos = parse_pos(gets.chomp)
-      rescue => exception
-        puts "Invalid position"
-        puts ""
-        
-        pos = nil
-      end
+    until pos && action_type && valid_pos?(pos) && valid_action?(action_type)
+      prompt_message
+
+      action_type, row, col = gets.chomp.split
+    
+      pos = [row.to_i, col.to_i]
     end
-    pos
+      [action_type, pos]
   end
 
-  def get_val
-    val = nil
-    until val && valid_val?(val)
-      puts "Enter your choice to either reveal('r') or flag('f')"
+  def prompt_message
+    puts "Enter an action move followed by a position on the board (e.g., e 3 4)"
+      puts
+      puts "Action moves: "
+      puts "'e' to explore"
+      puts "'f' to flag"
+      puts
       print "> "
-
-      begin
-        val = gets.chomp
-      rescue => exception
-        puts "Invalid options"
-        puts ""
-
-        val = nil
-      end
-    end
-    val
-  end
-
-  def parse_pos(pos)
-    pos.split.map{ |char| Integer(char) }
   end
 
   def valid_pos?(pos)
-    pos.is_a?(Array) && pos.length == 2 && pos.all?{ |x| x.between?(0, @grid_size - 1) }
+    pos.is_a?(Array) && pos.length == 2 && pos.all?{ |x| x.between?(0, board.grid_size - 1) }
   end
 
-  def valid_val?(val)
-    val.is_a?(String) && val.length == 1 && (val == 'f' || val == 'r')
+  def valid_action?(action)
+    action.is_a?(String) && action.length == 1 && (action == 'f' || action == 'e')
+  end
+
+  def perform_move(action_type, pos)
+    tile = board[pos]
+
+    case action_type
+    when 'f'
+      tile.toggle_flag
+    when  'e'
+      tile.explore   
+    end
   end
 end
